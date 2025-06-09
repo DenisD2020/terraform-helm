1c0b41cdd498ff991a255a41efb9168de6f81a36
VTB4049713 <dvrudenko@vtb.ru>
2025-06-09 17:05:19 +0300

    master add dev and prod env

--- dev/release.tf
+++ dev/release.tf
@@ -10,8 +10,19 @@ resource "helm_release" "nginx_release" {
   namespace = var.namespace_name
   chart     = "../charts/nginx"
   values = [
+    file("../charts/values-common.yaml"),
     file("../charts/values-${var.environment}.yaml")
   ]
   # Ждем создания namespace перед установкой
   depends_on = [kubernetes_namespace.app_namespace]
+}
+
+resource "helm_release" "hello_world" {
+  name       = "hello-world-dev"
+  namespace = var.namespace_name
+  chart      = "../charts/hello-world"
+  values = [
+    file("../charts/values-common.yaml"),
+    file("../charts/values-${var.environment}.yaml")
+  ]
 }
